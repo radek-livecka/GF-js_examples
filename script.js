4950a0ee087db6b47fabb2c4b7a9bdfdfbd3e80b
radek-livecka <radek-livecka@users.noreply.github.com>
2020-11-26 22:04:29 +0100

    added js content

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 console.log("This is javascript. Is it working")
+
+let post = {
+    title: "My post",
+    description: "My first post",
+    likeCount: 12,
+    comments: ["cool", "nice job"],
+}; 
+
+let colors = ["orange", "blue", "red", "black"]
+
+console.log(colors);
+
+console.log(post.title);
+
+let age = 18;
+
+if (age >= 18) {
+    console.log("you are ok");
+}
+else{
+    console.log("you are too young!");
+}
+
+for (let i = 0; i < 10;  i++) {
+    console.log(i);
+    
+}
+
+colors.forEach(prdel => {
+    console.log(prdel);    
+});
+
+let name = ["Petr", "Jan", "Karel"]
+
+name.forEach(x => {
+    console.log("Nazdar " + x);
+})
+
+let pozdrav = name =>{
+    console.log(`Ahoj ${name}`);
+}
+
+pozdrav("Pepa")
+pozdrav("Honza")
+
+// Jquery
+
+// $(selector).action()
+
+// $("div").text("hello world")
+
+let piv = [".first", ".second", ".fifth"]
+
+$(".btn").click(() => {
+    $(piv[0]).toggleClass("fifth");
+}
+)
+
+console.log(piv[0])
