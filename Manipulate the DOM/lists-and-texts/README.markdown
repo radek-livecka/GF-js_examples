48b66a6640ce69923b40e9aa47b538a60c73b4ee
radek-livecka <radek-livecka@users.noreply.github.com>
2020-11-28 20:57:30 +0100

    Manipulate the DOM
    exercises

---
+++ Manipulate the DOM/lists-and-texts/README.markdown
@@ -0,0 +1,5 @@
+# Lists and texts
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/radek_livecka/pen/XWKGvQP](https://codepen.io/radek_livecka/pen/XWKGvQP).
+
+
---
+++ Manipulate the DOM/script.js
@@ -0,0 +1,19 @@
+let names = ["Karel", "Pepa", "Jan", "Radek"]
+
+names.forEach(jmeno => {
+    $("body").append(`<ul class="${jmeno}">${jmeno}</ul>`);
+    if (jmeno === "Radek") {
+        $(`.Radek`).css("font-weight, strong");
+    }
+});
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+  };
+
+  $("body").append(`<div id="new"></div>`);  
+
+  $("#new").append(`<h1>${additionalBlock.title}</h1>`);
+
+  $("#new").append(`<p>${additionalBlock.text}</p>`);
