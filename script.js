a7d37fc1fcf0decc695763c4bc06b281c3258c7c
radek-livecka <radek-livecka@users.noreply.github.com>
2020-11-28 17:22:04 +0100

    Update script.js
    Basic building blocks in JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 console.log("This is javascript. Is it working")
 
+let author = "Radek Livečka"
+
+console.log(author);
+
 let post = {
     title: "My post",
     description: "My first post",
@@ -72,3 +76,69 @@ if (names[1].length <= 4 ) {
 names.forEach(x => {
     console.log(x)
 });
+
+let barvy = {
+    title: "Barvy",
+    seznam: ["green", "blue", "red", "yellow"],
+    count: 4,
+}
+
+
+// barvy.forEach(xx => {
+//     console.log(xx);
+// });
+
+let jablka = 4;
+
+for (let i = 0; i < jablka; i++) {
+    console.log(i);  
+}
+
+for (let i = 0; i < 21; i++) {
+    console.log(i);
+    
+}
+
+let add = (a, b) => {
+    for (a ; a <= b; a++) {
+         console.log(a);
+        
+    }
+  };
+  
+  add(1, 15)
+
+ let printNumberTill = (x) => {
+     for (let i = 1; i <= x; i++) {
+         console.log(i);
+         
+     }
+
+ }
+
+ printNumberTill(20)
+
+
+
+ let getGreetingTo = (x) => {
+     console.log("Hello " + x);
+ }
+
+ getGreetingTo("Mark")
+
+ let seznam1 = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+ let printValues = seznam1 => {
+     for (let i = 0; i < seznam1.length; i++){
+         console.log(seznam1[i])
+     }
+ }
+
+ printValues([0 ,3, 6, 7, 9]);
+
+ seznam1.forEach((number) => {
+     console.log(number);
+ });
+
+ let myNumbers = [10, 20, 30, 50, 12];
+
