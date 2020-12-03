84d98e1dddddae90d50d05e27b071a65c0b97062
radek-livecka <radek-livecka@users.noreply.github.com>
2020-12-03 21:28:51 +0100

    lots of bullshits added

--- My first web application/script.js
+++ My first web application/script.js
@@ -60,38 +60,84 @@ let imagesData = [{
     description: 'What happened here, why is this a very nice image'
 }]
 
-// $('#photo').attr('src', data[1].photo);
-// $('#photo-title').text(data.title);
-// $('#photo-description').text(data.description);
+
+imagesData.forEach((blbost, key) => {
+    //console.log(`key value: ${key} | blbost Name:`, blbost);
+    // console.log(imagesData[key].photo);
+    
+    $(".thumbnail").append(`<div class="thmb" data-number="${key}"><img id="${key}"</div>`);
+    $(`#${key}`).attr("src", imagesData[key].photo);
+    
+  });
+
+  //$(`data-number=${polozka}`).on(`click`, function() {
+  //    currentPhoto = `${polozka}`;
+  //});
+
+  $(".thmb").on("click", function() {
+      var dataNumber = $(this).attr("data-number");
+      $("#photo").attr("src", imagesData[dataNumber].photo);
+  })
+
 
 let currentPhoto = 0;
-$("#photo").attr("src", imagesData[currentPhoto].photo);
-$('#photo-title').text(imagesData[currentPhoto].title);
-$('#photo-description').text(imagesData[currentPhoto].description);
+
 
 let loadPhoto = function (photoNumber) {
     $("#photo").attr("src", imagesData[photoNumber].photo);
     $('#photo-title').text(imagesData[photoNumber].title);
     $('#photo-description').text(imagesData[photoNumber].description);
+    $(`[data-number="${photoNumber}"]`).removeClass("thmb").addClass("thmbsel");
+    console.log(photoNumber);
     
 };
 
 loadPhoto(currentPhoto);
 
-$("#larrow").on("click", function() {
+let posunVlevo = function(){
+/* $("#larrow").on("click", function() { */
     if (currentPhoto > 0) {
         currentPhoto --;
     } else {
         currentPhoto = 12;
     }
     loadPhoto(currentPhoto);
-});
+    /* $(".img").fadeOut(1600).fadeIn(1600); */
+};
 
-$("#rarrow").on("click", function() {
+$("#larrow").on("click", posunVlevo);
+
+
+let posunVpravo = function(){
+/* $("#rarrow").on("click", function() { */
     if (currentPhoto < 12) {
         currentPhoto ++;
     } else {
         currentPhoto = 0;
     }
     loadPhoto(currentPhoto);
+};
+
+$("#rarrow").on("click", posunVpravo);
+
+let leftArrow = function(){
+    (keyCode || which) === 37;
+};
+
+let rightArrow = function(){
+    (keyCode || which) === 39;
+};
+
+$(document).keydown(function(){
+    // left arrow
+    if (leftArrow)
+    {   
+       $(posunVlevo);
+    }
+    // right arrow
+    else 
+    {
+        {if (rightArrow)
+        $(posunVpravo);}
+    }   
 });
---
+++ My first web application/script_old_n_notes.js
@@ -0,0 +1,53 @@
+// řádek 63 a dál
+// $('#photo').attr('src', data[1].photo);
+// $('#photo-title').text(data.title);
+// $('#photo-description').text(data.description);
+
+// let loadThumb = function(thumbNumber) {
+//     $(".thmb").attr("src", imagesData[thumbNumber].photo)
+// }
+
+// let addThumb = function() {
+//     $(".thumbnail").append(`<div data-number="${thumbNumber}"></div>`)
+// }
+
+// let pocetFotek = for (let i = 0; i <= imagesData.length; i++) {
+//     return i;
+//     // $(".thumbnail").append(`<div class="thmb" data-number="4"><img id="`${pocetFotek}`"></div>`);   
+// };
+
+
+// let pocetFotek = function(){
+//     imagesData.length;
+//     console.log(pocetFotek);
+// };
+
+// let key = Object.keys(imagesData[1]);
+// console.log(key)
+
+// let valu = Object.values(imagesData);
+// console.log(valu);
+
+// valu.forEach(element => { console.log(element)});
+
+// let entr = Object.entries(imagesData);
+// console.log(entr[0].photo);
+
+// let entr2 = Object.entries(entr);
+// console.log(entr2);
+
+// let valu2 = Object.values(valu);
+// console.log(valu2);
+
+// for(let i = ; i < imagesData.length; i++){
+//     console.log(`i value: ${i} | name: `, imagesData[i]);
+// }
+
+// řádek 80 a dál
+// $("#photo").attr("src", imagesData[currentPhoto].photo);
+// $('#photo-title').text(imagesData[currentPhoto].title);
+// $('#photo-description').text(imagesData[currentPhoto].description);
+
+// řádek 74 a dál
+   // $(".thumbnail").append(`<div class="thmb" data-number="4"><img id="4"></div>`);
+    // $("#4").attr("src", imagesData[4].photo);
